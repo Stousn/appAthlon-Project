658bd4c07fcfdb144dc2a9ceb8e8574226505f23
Stousn <stefan.reip@edu.fh-joanneum.at>
2016-12-01 14:39:54 +0100

    Add Activity activity added :D

--- FitAppIndoor/app/src/main/java/at/fhj/appathlon/fitapp/fitappindoor/app/view/MainActivity.java
+++ FitAppIndoor/app/src/main/java/at/fhj/appathlon/fitapp/fitappindoor/app/view/MainActivity.java
@@ -13,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import at.fhj.appathlon.fitapp.fitappindoor.R;
 
@@ -30,8 +31,8 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                addActivity();
             }
         });
 
@@ -106,5 +107,10 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void addActivity() {
+        Intent i = new Intent(this,AddActivityActivity.class);
+        startActivity(i);
     }
 }
