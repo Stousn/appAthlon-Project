2f66dcfcf580f334e91d8c650545ed571cf41530
Stousn <stefan.reip@edu.fh-joanneum.at>
2016-12-02 14:40:12 +0100

    missing import added

--- FitAppIndoor/app/src/main/java/at/fhj/appathlon/fitapp/fitappindoor/app/view/MainActivity.java
+++ FitAppIndoor/app/src/main/java/at/fhj/appathlon/fitapp/fitappindoor/app/view/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
